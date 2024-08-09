7e8611e3c224646dfdc0bf68acb71ac8864c24cc
Pedro Belo <pn999717@gmail.com>
2024-08-08 21:36:43 -0300

    fix: permitir comunicação com localhost

--- lib/api.dart
+++ lib/api.dart
@@ -11,9 +11,9 @@ class EnvironmentConfig {
   static createFrom(String environment) {
     switch (environment) {
       case "development":
-        return "http://localhost:8000";
+        return "http://10.0.2.2:8000";
       case "production":
-        return "https://127.0.0.1";
+        return "http://127.0.0.1";
       default:
         throw ArgumentError(environment);
     }
